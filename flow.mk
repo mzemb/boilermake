d15b7f0fe8182530fc53871f32160f0484bbf4a7
Michael Zemb <michael.zemb@gmail.com>
2017-01-26 20:37:25 +0100

    add STR_LEN to gitHash generated version string

--- flow.mk
+++ flow.mk
@@ -8,32 +8,33 @@
 target/gitHash.h:
 	@echo "# making $@"
 	@mkdir -p $(dir $@)
-	$(AT)tmpRev=target/gitHash.tmp                                ;\
-	echo "#pragma once"                               > $$tmpRev &&\
-	echo -n '#define GIT_HASH_STR "'                 >> $$tmpRev &&\
-	/usr/bin/git log --pretty=format:'%h' -n 1                     \
-			| tr --delete '\n'                       >> $$tmpRev  ;\
-	set +e                                                        ;\
-	/usr/bin/git diff-index --quiet HEAD                          ;\
-	diffs=$$?                                                     ;\
-	set -e                                                        ;\
-	if [ "$$diffs" != "0" ]; then                                  \
-	    echo -n '-DIRTY'                             >> $$tmpRev  ;\
-	else                                                           \
-	    echo -n '-CLEAN'                             >> $$tmpRev  ;\
-	fi                                                           &&\
-	echo '"'                                         >> $$tmpRev &&\
-	echo ''                                          >> $$tmpRev &&\
-	echo '#define GIT_HASH_LEN STRLEN(GIT_HASH_STR)' >> $$tmpRev &&\
-	if [ ! -f $@ ]; then                                           \
-	    new=1                                                     ;\
-	else                                                           \
-		set +e                                                    ;\
-		diff $$tmpRev $@ 1>/dev/null                              ;\
-		new=$$?                                                   ;\
-		set -e                                                    ;\
-	fi                                                           &&\
-	if [ "$$new" != "0" ]; then                                    \
-	    cp $$tmpRev $@                                            ;\
+	$(AT)tmpRev=target/gitHash.tmp                                 ;\
+	echo "#pragma once"                                > $$tmpRev &&\
+	echo "#define STRLEN(s) (sizeof(s)/sizeof(s[0]))" >> $$tmpRev &&\
+	echo -n '#define GIT_HASH_STR "'                  >> $$tmpRev &&\
+	/usr/bin/git log --pretty=format:'%h' -n 1                      \
+			| tr --delete '\n'                        >> $$tmpRev  ;\
+	set +e                                                         ;\
+	/usr/bin/git diff-index --quiet HEAD                           ;\
+	diffs=$$?                                                      ;\
+	set -e                                                         ;\
+	if [ "$$diffs" != "0" ]; then                                   \
+	    echo -n '-DIRTY'                              >> $$tmpRev  ;\
+	else                                                            \
+	    echo -n '-CLEAN'                              >> $$tmpRev  ;\
+	fi                                                            &&\
+	echo '"'                                          >> $$tmpRev &&\
+	echo ''                                           >> $$tmpRev &&\
+	echo '#define GIT_HASH_LEN STRLEN(GIT_HASH_STR)'  >> $$tmpRev &&\
+	if [ ! -f $@ ]; then                                            \
+	    new=1                                                      ;\
+	else                                                            \
+		set +e                                                     ;\
+		diff $$tmpRev $@ 1>/dev/null                               ;\
+		new=$$?                                                    ;\
+		set -e                                                     ;\
+	fi                                                            &&\
+	if [ "$$new" != "0" ]; then                                     \
+	    cp $$tmpRev $@                                             ;\
 	fi
 
--- src/hello.c
+++ src/hello.c
@@ -2,10 +2,11 @@
 #include "target/gitHash.h"
 
 int main (void){
-    printf( "hello world, I'm %s build on %s from %s\n"
+    printf( "hello world, I'm %s build on %s from %s (len=%u)\n"
     ,   __FILE__
     ,   __DATE__
     ,   GIT_HASH_STR
+    ,   (unsigned)GIT_HASH_LEN
     );
     return 0;
 }
